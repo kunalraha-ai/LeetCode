4d38d9f86552f63663d07e3352eec85bc4b09df7
Kunal Raha <kunal.raha.ai@gmail.com>
2026-07-30 13:12:17 +0530

    Time: 11 ms (74.88%) | Memory: 32.4 MB (22.09%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,6 +1,6 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        seen = set()
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen=set()
         for num in nums:
             if num in seen:
                 return True
